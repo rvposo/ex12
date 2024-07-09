4e53b7fdd490e4daba3b04b60531eae8fd50ea99
Gabriel Raposo <gabrielraposo0@hotmail.com>
2024-07-09 19:22:20 -0300

    Adicionado algumas variaveis

--- Program.cs
+++ Program.cs
@@ -1,9 +1,34 @@
-﻿namespace ex12;
+﻿/* Escrever um algoritmo que leia informações sobre um grupo de 250 pessoas e calcule alguns dados estatísticos. 
+Para cada pessoas do grupo deve ler o nome da pessoa, a altura, o peso e o sexo (“F” para feminino e “M” para o masculino). 
+Calular e escrever:
+A quantidade total de homens e mulheres e o percentual de cada.
+A média de peso das pessoas (somatório dos pesos de todas as pessoas pela quantidade de pessoas) o nome da pessoa mais alta. */
+
+namespace ex12;
 
 class Program
 {
     static void Main(string[] args)
     {
-        
+         int quantidadePessoas = 10;
+         string nome = "";
+         double altura = 0f;
+
+
+
+         for (int i = 0; i <= quantidadePessoas; i++)
+         {
+            Console.WriteLine("Digite o nome da pessoa: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a altura: ");
+            altura = Convert.ToDouble(Console.ReadLine());
+
+            
+         }
+
+
+
+                 
     }
 }
